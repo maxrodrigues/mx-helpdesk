1e0e15d1bb966c3d58f8c4f5200c0f380001272e
Max Rodrigues <maxuel.rodrigues@gmail.com>
2024-07-26 17:33:17 -0300

    feat: update edit ticket view

--- resources/views/pages/tickets/edit.blade.php
+++ resources/views/pages/tickets/edit.blade.php
@@ -28,20 +28,23 @@
                     <label>Prioridade</label>
                     <select class="w-full text-sm rounded border border-gray-300 focus:ring-0 focus:border-gray-300">
                         <option>.: Selecione :.</option>
-                        <option></option>
-                        <option></option>
-                        <option></option>
+                        <option value="1">Urgente</option>
+                        <option value="2">Alta</option>
+                        <option value="3">Média</option>
+                        <option value="4">Baixa</option>
                     </select>
                 </div>
             </div>
             <div>
                 <div class="flex justify-between items-end">
                     <label>Responsável</label>
-                    <a href="#">
+                    @if(! $agent || $agent->id != $me->id)
+                    <a href="{{ route('ticket.assign-me', ['ticket' => $ticket->id]) }}">
                         <span class="text-xs text-blue-700">Atribuir a mim</span>
                     </a>
+                    @endif
                 </div>
-                <input type="text" class="w-full text-sm rounded border border-gray-300 focus:ring-0 focus:border-gray-300">
+                <input type="text" class="w-full text-sm rounded border border-gray-300 focus:ring-0 focus:border-gray-300" @if($agent) value="{{ $agent->name }}" @endif/>
             </div>
             {{--<div>
                 <label>Tags</label>
@@ -53,11 +56,11 @@
                 <label>Assunto</label>
                 <input type="text" class="w-full text-sm rounded bg-gray-200 border border-gray-300 text-gray-400" value="{{ $ticket->title }}" disabled="disabled"/>
             </div>
-            <form action="{{ route('add-reply', ['ticket' => $ticket->id]) }}" method="post">
+            <form action="{{ route('ticket.add-reply', ['ticket' => $ticket->id]) }}" method="post">
                 @csrf
                 <div class="mb-5">
                     <label>Reponder</label>
-                    <textarea name="reply" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded border border-gray-300 focus:ring-blue-500 focus:border-blue-500" placeholder="Write your thoughts here..."></textarea>
+                    <textarea name="reply" rows="6" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded border border-gray-300 focus:ring-blue-500 focus:border-blue-500" placeholder="Write your thoughts here..."></textarea>
                     @error('reply')
                     <span class="text-red-500 text-xs">{{ $message }}</span>
                     @enderror
@@ -67,10 +70,11 @@
                 </button>
             </form>
             <hr class="h-px my-2 bg-gray-200 border-0" />
-            <p>{{ $ticket->description }}</p>
+            <p class="text-sm">{{ $ticket->description }}</p>
             <hr class="h-px mt-5 bg-gray-200 border-0" />
+            @if(! empty($replies))
             <ol class="relative border-s-2 border-gray-300 m-5">
-                @forelse($replies as $reply)
+                @foreach($replies as $reply)
                     <li class="mb-10 ms-6">
                         <span class="absolute flex items-center justify-center w-8 h-8 rounded-full -start-4 ring-8 ring-white">
                             <img src="{{ asset('img/avatar.jpg') }}" class="rounded-full"/>
@@ -81,12 +85,13 @@
                             <p class="mb-4 text-sm font-normal text-gray-500">{{ $reply->content }}</p>
                         </div>
                     </li>
-                @empty
-                    <span class="text-xs text-gray-400">
-                        Nenhuma resposta adicionada
-                    </span>
-                @endforelse
+                @endforeach
             </ol>
+            @else
+                <span class="text-xs text-gray-400">
+                    Nenhuma resposta adicionada
+                </span>
+            @endif
         </div>
     </div>
 @endsection
