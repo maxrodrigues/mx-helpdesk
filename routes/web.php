3f50a3a9dcd16ed69e5f57b209ca39989e79f324
Max Rodrigues <maxuel.rodrigues@gmail.com>
2024-07-26 17:33:44 -0300

    feat: add new routes

--- routes/web.php
+++ routes/web.php
@@ -9,4 +9,5 @@ Route::get('/', function () {
 });
 
 Route::resource('tickets', \App\Http\Controllers\TicketController::class);
-Route::post('add-reply/{ticket}', \App\Http\Controllers\AddReplyController::class)->name('add-reply');
+Route::post('add-reply/{ticket}', \App\Http\Controllers\Ticket\AddReplyController::class)->name('ticket.add-reply');
+Route::get('assign-me/{ticket}', \App\Http\Controllers\Ticket\SetAgentController::class)->name('ticket.assign-me');
