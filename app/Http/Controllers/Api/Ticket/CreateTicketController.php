d189582afcd0b8e760c4ca3a0efd43f0e2edaf08
Max Rodrigues <maxuel.rodrigues@gmail.com>
2024-07-29 16:57:35 -0300

    feat: add request validation

--- app/Http/Controllers/Api/Ticket/CreateTicketController.php
+++ app/Http/Controllers/Api/Ticket/CreateTicketController.php
@@ -5,28 +5,29 @@ namespace App\Http\Controllers\Api\Ticket;
 use App\Enum\TicketPriorityEnum;
 use App\Enum\TicketStatusEnum;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\Ticket\CreateTicketRequest;
 use App\Models\Customer;
 use App\Models\Ticket;
 use Exception;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class CreateTicketController extends Controller
 {
-    public function __invoke(Request $request): JsonResponse
+    public function __invoke(CreateTicketRequest $request): JsonResponse
     {
         try {
-            $customer = Customer::where('uuid', $request->customer)->first();
+            $data = $request->validated();
+            $customer = Customer::where('uuid', $data['customer'])->first();
             if (! $customer) {
                 $customer = Customer::create([
-                    'uuid' => $request->customer,
+                    'uuid' => $data['customer'],
                 ]);
             }
 
             $ticket = Ticket::create([
-                'title' => $request->title,
-                'description' => $request->description,
+                'title' => $data['title'],
+                'description' => $data['description'],
                 'status' => TicketStatusEnum::OPEN,
                 'priority' => TicketPriorityEnum::LOW,
                 'customer_id' => $customer->id,
